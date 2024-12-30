e17fe03936c3ccb96ff2901f7b3d062177c3e580
Henry Anderson <henryanderson@Henrys-MacBook-Air.local>
2024-12-30 14:04:55 -0700

    Updated index.html

--- projects/8ballmagic/script.js
+++ projects/8ballmagic/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var magic8Ball = {};
-    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "I say this with full certanty, possibly!", "Of course!", "for sure!!!", "In your dreams.", "Not sure. maybe I guess?"];
   
     $("#answer").hide();
   
